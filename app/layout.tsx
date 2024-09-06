963638ff7fa4b0d1d46356e528789c2f13166122
GitBugKing <94054799+GitBugKing@users.noreply.github.com>
2024-09-05 19:18:49 -0700

    Upload full web pic and add og in meta data to make social media recognize my web

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,21 @@ import {ThemeProvider} from "@/components/theme-provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Welcome To My Portfolop Site | Vincent Liu",
-  description: "My Coding Chronicles",
+  title: "Welcome To My Portfolio Site | Vincent Liu",
+  openGraph: {
+    title: "Welcome To My Portfolio Site | Vincent Liu",
+    description: "My Coding Chronicles",
+    type: "website",
+    url: "https://de-portfolio.vercel.app/", 
+    images: [
+      {
+        url: "https://de-portfolio.vercel.app/public/webpic.jpg", 
+        width: 1200,
+        height: 630,
+        alt: "Vincent Liu's Portfolio Cover",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
